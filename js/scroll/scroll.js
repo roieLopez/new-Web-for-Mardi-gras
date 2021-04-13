69eb0cfe7060d7aa4f3d818b8f7d6957e58fc1a3
roilopezZ <64579967+roilopezZ@users.noreply.github.com>
2021-04-13 22:38:35 +0300

    Add files via upload

--- js/scroll/scroll.js
+++ js/scroll/scroll.js
@@ -1,21 +1,18 @@
-$(document).on('scroll', function() {
+$(document).on('scroll', function () {
   // console.log($(this).scrollTop());
 
   if ($(this).scrollTop() == 0) { //if(document == 0){}
-      $('.titleWeOffer').removeClass('title');
-      $('.containerMenu').removeClass('animationMenu');
-      $('.containerIframeRight').removeClass('animationIframeRight');
-      $('.containerIframe').removeClass('animationIframeLeft');
-      $('.animationAbout').removeClass('animationMenuAbout');
-      $('.video').removeClass('videoAnim');
+    $('.titleWeOffer').removeClass('title');
+    $('.containerMenu').removeClass('animationMenu');
+    $('.containerIframeRight').removeClass('animationIframeRight');
+    $('.containerIframe').removeClass('animationIframeLeft');
+    $('.video').removeClass('videoAnim');
   } else {
-      $('.titleWeOffer').addClass('title');
-      $('.titleWeOffer').css("display","block");
-      $('.containerMenu').addClass('animationMenu');
-      $('.containerIframeRight').addClass('animationIframeRight');
-      $('.containerIframe').addClass('animationIframeLeft');
-      $('.animationAbout').addClass('animationMenuAbout');
-      $('.video').addClass('videoAnim');
+    $('.titleWeOffer').addClass('title');
+    $('.titleWeOffer').css("display", "block");
+    $('.containerMenu').addClass('animationMenu');
+    $('.containerIframeRight').addClass('animationIframeRight');
+    $('.containerIframe').addClass('animationIframeLeft');
+    $('.video').addClass('videoAnim');
   }
 });
-
